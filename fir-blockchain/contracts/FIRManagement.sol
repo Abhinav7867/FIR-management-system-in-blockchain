28f149cd026658f20152031112e6773aa9e03105
Abhinav Jha <itisbpjha@hotmail.com>
2025-01-31 14:52:25 +0530

    fixes
    Gas limit of ganache and hardhat fixes

--- fir-blockchain/contracts/FIRManagement.sol
+++ fir-blockchain/contracts/FIRManagement.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 contract FIRManagement {
     struct FIR {
@@ -18,8 +18,17 @@ contract FIRManagement {
 
     // Function to file a new FIR
     function fileFIR(string memory _description) public {
+        require(bytes(_description).length > 0, "Description cannot be empty");
+        
         firCount++;
-        firs[firCount] = FIR(firCount, _description, msg.sender, block.timestamp, "Filed");
+        firs[firCount] = FIR({
+            id: firCount,
+            description: _description,
+            complainant: msg.sender,
+            timestamp: block.timestamp,
+            status: "Filed"
+        });
+
         emit FIRFiled(firCount, msg.sender);
     }
 
@@ -28,12 +37,15 @@ contract FIRManagement {
         require(_id > 0 && _id <= firCount, "FIR does not exist");
         FIR storage fir = firs[_id];
         require(fir.complainant == msg.sender, "Only complainant can update FIR");
+        require(bytes(_status).length > 0, "Status cannot be empty");
+
         fir.status = _status;
         emit FIRUpdated(_id, _status);
     }
 
     // Function to get the details of an FIR
     function getFIR(uint _id) public view returns (uint, string memory, address, uint, string memory) {
+        require(_id > 0 && _id <= firCount, "FIR does not exist");
         FIR storage fir = firs[_id];
         return (fir.id, fir.description, fir.complainant, fir.timestamp, fir.status);
     }
